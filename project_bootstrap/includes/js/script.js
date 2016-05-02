88d557df239a30e2b1893488238e5920d28086e3
sr326322 <yarramati.srikanth@gmail.com>
2016-05-02 16:47:33 +0530

    bootstrap_submission

---
+++ project_bootstrap/includes/js/script.js
@@ -0,0 +1,19 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+$(function()
+{
+  $('a.pop').click(fuction(e)
+{
+  e.preventDefault();
+});
+$('a.pop').popover();
+});
